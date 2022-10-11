07a62a173c4a39eb6ae6af2f0080b88bed00d080
trudyvianai <107434432+trudyvianai@users.noreply.github.com>
2022-10-11 14:55:58 -0400

    added extra browsing info to cards

--- src/cards.js
+++ src/cards.js
@@ -12,8 +12,21 @@ $(document).ready(function () {
         `<section id=${json["cards"][i]["id"]}> 
             <h3>${json["cards"][i]["title"]} </h3>
             <p>${json["cards"][i]["description"]} </p>
+            <div class="container" id="${json["cards"][i]["id"]}-div">  </div>
         </section>`
       );
+
+      let cardDiv = $(`#${json["cards"][i]["id"]}-div`);
+      for (let j = 0; j < json["cards"][i]["browser"].length; j++) {
+        cardDiv.append(
+          `<div >
+            ${json["cards"][i]["browser"][j]["description"]}
+ 
+            </div>`
+        );
+      }
     }
   });
 });
+
+$();
